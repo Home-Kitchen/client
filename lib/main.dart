2c4c9daace756fe878f6d7fddea5aad926d5d163
Visakh Vijayan <visakh.vijayan@seenit.in>
2020-03-27 20:15:18 +0530

    Visakh - changes for createorganization and createuser - [VV]

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import "package:flutter/material.dart";
+import 'package:kitchen/components/CreateOrganization.dart';
+import 'package:kitchen/components/CreateUser.dart';
 
 void main()
 {
-	runApp();
+	runApp(MaterialApp
+	(
+		debugShowCheckedModeBanner: false,
+		title: "Kitchen",
+		initialRoute: "/",
+		routes: 
+		{
+			"/": (context) => CreateOrganization(),
+			"/createUser": (context) => CreateUser()
+		},
+	));
 }
