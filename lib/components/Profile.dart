242691b52896b1da9163aa65d9490c493282c660
amithamohan <damitha.mohan@gmail.com>
2020-03-29 11:16:50 +0530

    Done

---
+++ lib/components/Profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatefulWidget
+{
+	createState()
+	{
+		return ProfileState();
+	}
+}
+
+class ProfileState extends State<Profile>
+{
+	Widget build(BuildContext context)
+	{}
+}
